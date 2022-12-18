7930262b21f29f10493645b570ae7d4c917b9121
Adam <chillcoders@gmail.com>
2022-12-18 14:18:43 -0500

    db-thangs

--- index.ts
+++ index.ts
@@ -4,23 +4,21 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-
-//   read all the users
+  //   read all the users
   //   const allUsers = await prisma.user.findMany()
-//   console.log(allUsers)
-
-await prisma.counter_value.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      posts: {
-        create: { title: 'Hello World' },
-      },
-      profile: {
-        create: { bio: 'I like turtles' },
-      },
-    },
-
+  //   console.log(allUsers)
+  // write new records via create
+  // await prisma.user.create({
+  //     data: {
+  //       name: 'Alice',
+  //       email: 'alice@prisma.io',
+  //       posts: {
+  //         create: { title: 'Hello World' },
+  //       },
+  //       profile: {
+  //         create: { bio: 'I like turtles' },
+  //       },
+  //     },
 }
 
 main()
